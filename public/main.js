12b02ec54205e2354045962ffd146a94834a2103
Kai <1114366961@qq.com>
2020-12-17 19:56:16 +0800

    AJAX（上） 请求css,js，html,xml,json以及实现最基础的分页

---
+++ public/main.js
@@ -0,0 +1,99 @@
+// 
+let n = 1;
+getPage.onclick =()=>{
+    const request = new XMLHttpRequest
+    request.open("GET",`/page${n+1}`)
+    request.onreadystatechange=()=>{
+        if(request.readyState ===4 && request.status ===200){     
+                const array = JSON.parse(request.response)
+                array.forEach(item => {
+                    const li = document.createElement("li")
+                    li.textContent = item.id
+                    xxx.appendChild(li)  
+                })    
+                n+=1
+        }
+    }
+    request.send()
+}
+
+
+
+getJSON.onclick=()=>{
+    const request= new XMLHttpRequest
+    request.open('GET','/5.json')
+    request.onreadystatechange=()=>{
+        if(request.readyState===4){
+            if(request.status>=200&& request.status<300){
+                console.log(request.response)
+                const object= JSON.parse(request.response)
+                console.log(object)
+                myName.innerHTML=object.name
+               
+            }else{
+                alert('加载错误')
+            }
+        }
+    }
+    request.send()
+}
+
+getXML.onclick=()=>{
+    const request =new XMLHttpRequest()
+    request.open('GET','/4.xml')
+    console.log(request.response)
+    request.onreadystatechange=()=>{
+        if(request.readyState===4){
+            if(request.status>=200&&request.status<300){
+                console.log(request.responseXML)
+              const dom= request.responseXML
+                const text=dom.getElementsByTagName('warning')[0].textContent
+                console.log(text.trim())
+            }else{
+                alert('错了')
+            }
+        }
+    }
+    request.send()
+}
+getHTML.onclick=()=>{
+    const request = new XMLHttpRequest()
+    request.open('GET','/3.html')
+    request.onload=()=>{
+        console.log(request.response)
+        const div = document.createElement('div')
+        div.innerHTML = request.response
+        document.body.appendChild(div)
+    }
+    request.onerror=()=>{}
+    request.send()
+}
+getJS.onclick=()=>{
+    const request = new XMLHttpRequest()
+    request.open('GET','/2.js')
+    request.onload=()=>{
+    const script = document.createElement('script')
+    script.innerHTML = request.response
+    document.body.appendChild(script)
+    }
+    request.onerror=()=>{}
+    request.send()
+}
+getCss.onclick =()=>{
+    const request = new XMLHttpRequest()
+    request.open('GET','/style.css')
+    request.onreadystatechange=()=>{
+        if(request.readyState===4 ){
+            if(request.status>=200 && request.status<300){
+                    const style =document.createElement('style')
+                    style.innerHTML =request.response
+                    document.head.appendChild(style)
+                }else{
+                    alert('加载失败')
+                }
+        }
+    }
+    request.send()
+}
+
+
